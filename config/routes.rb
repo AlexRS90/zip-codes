a0e5c01e51341189a0c5c6c70d78496fe240d6a4
AlexRS90 <aramosbengy@gmail.com>
2022-04-10 14:46:39 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'api/v1/zip-codes/:zip_code', to: 'codes#show'
+      get 'zip-codes/:zip_code', to: 'codes#show'
     end
   end
 end
