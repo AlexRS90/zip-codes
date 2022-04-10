ba10fbd229a40602b49d89acef5f0c21ba243eff
AlexRS90 <aramosbengy@gmail.com>
2022-04-10 14:42:50 -0500

    Create route to API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'codes/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      get 'api/v1/zip-codes/:zip_code', to: 'codes#show'
+    end
+  end
 end
