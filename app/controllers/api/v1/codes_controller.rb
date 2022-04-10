bae4ad4cea77f5f9696a990a0c08289d2505e080
AlexRS90 <aramosbengy@gmail.com>
2022-04-10 14:57:11 -0500

    Delete extra method definition

--- app/controllers/api/v1/codes_controller.rb
+++ app/controllers/api/v1/codes_controller.rb
@@ -1,26 +1,24 @@
 class Api::V1::CodesController < ApplicationController
   def show
-    def show
-      zip_code = Code.where(zip_code: params[:zip_code]).pluck(
-        :zip_code,
-        :locality,
-        :federal_entity,
-        :settlements,
-        :municipality
-      ).map do |zip_code1, locality, federal_entity, settlements, municipality|
-        {
-          zip_code: zip_code1,
-          locality: locality,
-          federal_entity: federal_entity,
-          settlements: settlements,
-          municipality: municipality
-        }
-      end
-      if zip_code.any?
-        render json: { status: 'success', data: zip_code }, status: 200
-      else
-        render json: {}, status: :not_found
-      end
+    zip_code = Code.where(zip_code: params[:zip_code]).pluck(
+      :zip_code,
+      :locality,
+      :federal_entity,
+      :settlements,
+      :municipality
+    ).map do |zip_code1, locality, federal_entity, settlements, municipality|
+      {
+        zip_code: zip_code1,
+        locality: locality,
+        federal_entity: federal_entity,
+        settlements: settlements,
+        municipality: municipality
+      }
+    end
+    if zip_code.any?
+      render json: { status: 'success', data: zip_code }, status: 200
+    else
+      render json: {}, status: :not_found
     end
   end
 end
